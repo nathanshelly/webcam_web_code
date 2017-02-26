39e5f35c716c0f9583cd8517370c3372851d3b98
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2017-02-25 21:54:57 -0800

    nav bar

--- webcam/scripts/webcam_functions.js
+++ webcam/scripts/webcam_functions.js
@@ -86,3 +86,13 @@ function init() {
 
 // Open Websocket as soon as page loads
 window.addEventListener("load", init, false);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
